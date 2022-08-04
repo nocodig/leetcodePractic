11f8c9a7b2f74fe48829ff51c803cd4e9e1161b7
nocoding <17801149870@163.com>
2022-08-04 10:49:10 +0800

    feat: 🎸 二次初始化，增加ts

--- src/20211102/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: lifang
- * @Date: 2021-11-01 11:58:44
- * @LastEditTime: 2021-11-01 11:58:44
- * @LastEditors: lifang
- * @Description: 
- * @FilePath: /leetcodePractic/src/2021102/index.js
- */
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/*
+ * @Author: 李放
+ * @Date: 2022-08-04 10:21:58
+ * @Description: 
+ * @FilePath: /leetcodePractic/src/index.ts
+ */
